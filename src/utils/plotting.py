bf2c6875a6377e850a4a069b9b1e5442dc8b5998
Fabian Gröger <fabian.groeger@bluewin.ch>
2026-04-01 19:51:25 +0200

    fixed IT"

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -11,7 +11,6 @@ import scipy
 import seaborn as sns
 import torch
 import torch.nn.functional as F
-import umap
 from loguru import logger
 from matplotlib.gridspec import SubplotSpec
 from sklearn.metrics import auc
@@ -632,6 +631,8 @@ def embedding_plot(
 
     if X.shape[0] > 2:
         # apply if the dimension is larger than 2
+        import umap
+
         umap_transformer = umap.UMAP(
             n_components=2, n_neighbors=100, random_state=42, n_jobs=1
         )
@@ -721,6 +722,8 @@ def embedding_plot_w_markers(
 
     if X.shape[0] > 2:
         # apply if the dimension is larger than 2
+        import umap
+
         if text_X is not None:
             X = np.concatenate([text_X, X])
         umap_transformer = umap.UMAP(
